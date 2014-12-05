be22b2845aea85ea59ff9b2b105d359e913bfc6c
moodi <rajathvsm@gmail.com>
2014-12-05 02:45:03 -0800

    Adding a script file to gather data

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+python app.py modi >> modi.txt &
+python app.py obama >> obama.txt &
+python app.py philhughes >> philhughes.txt &
+python app.py chipotle >> chipotle.txt &
+python app.py mcdonalds >> mcdonalds.txt &
+python app.py ferguson >> ferguson.txt &
+python app.py pepsi >> pepsi.txt &
+python app.py cocacola >> cocacola.txt &
+python app.py india >> india.txt &
+python app.py china >> china.txt &
+python app.py usa >> usa.txt &
+python app.py pinkfloyd >> pinkfloyd.txt &
+python app.py taylorswift >> taylorswift.txt &
+python app.py kardashian >> kardashian.txt &
+python app.py federer >> federer.txt &
